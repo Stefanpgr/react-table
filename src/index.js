16cf9505ebe3f0924eef84aadeae474997a3621d
stefanpgr <stefanpongrz@gmail.com>
2019-10-28 15:16:59 +0100

    re-add redux_dev_tools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import tableReducer from "./reducers/tableReducer";
 
-const store = createStore(tableReducer);
+const store = createStore(
+  tableReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
